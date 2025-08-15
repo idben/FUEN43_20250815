8a38538a294e57dd84ade3f84dc876972cd2a973
Ben Chen <idben.chen@gmail.com>
2025-08-15 14:49:26 +0800

    table fetch

--- file2.js
+++ file2.js
@@ -1,3 +1,5 @@
+var tbody = document.querySelector("table tbody");
+
 loadData();
 
 async function loadData() {
@@ -7,6 +9,19 @@ async function loadData() {
         if (!response.ok) throw new Error(response.statusText);
         const result = await response.json();
         console.log(result);
+        let contentHTML = "";
+
+        result.forEach((value, index) => {
+            console.log(value["地區"]);
+            contentHTML += `<tr>
+                                <th scope="row">${index+1}</th>
+                                <td>${value["地區"]}</td>
+                                <td>${value["欄位名稱"]}</td>
+                                <td>${value["數值"]}</td>
+                            </tr>`;
+        });
+
+        tbody.innerHTML = contentHTML;
     } catch (error) {
         console.log(error);
 

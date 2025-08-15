fac1f61c861148bb17079a2dad09152c4a8e7b42
Ben Chen <idben.chen@gmail.com>
2025-08-15 13:55:44 +0800

    fetch 5

--- file1.js
+++ file1.js
@@ -1,4 +1,7 @@
 const navLinks = document.querySelectorAll(".nav2 .nav-link");
+const userH2 = document.querySelector(".navcontent h2");
+const userH3 = document.querySelector(".navcontent h3");
+const userImg = document.querySelector(".navcontent img");
 
 navLinks.forEach(function (navLink, index) {
     console.log(navLink);
@@ -10,5 +13,22 @@ navLinks.forEach(function (navLink, index) {
             document.querySelector(".nav2 .active").classList.remove("active");
         }
         navLink.classList.add("active");
+        loadUser();
     });
 });
+
+
+function loadUser() {
+    const url = "https://randomuser.me/api";
+    fetch(url).then(res => {
+        if (!res.ok) throw new Error(res.statusText);
+        return res.json();
+    }).then(result => {
+        console.log(result.results[0]);
+
+        const user = result.results[0];
+        userH2.innerHTML = `${user.name.title} ${user.name.first} ${user.name.last}`;
+        userImg.src = user.picture.large;
+        userH3.innerText = user.email;
+    }).catch(error => console.log(error));
+}
